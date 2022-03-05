1405d63ef31f7db09462ff45e2fb6716027860f9
Will Cavenagli <willcavenagli@gmail.com>
2022-03-05 12:18:03 -0300

    🧩: Add GetxHomePresenter Implementation

---
+++ lib/src/presentation/presenters/home_presenter/getx_home_presenter.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_tv_maze_jobsity/src/presentation/presenters/home_presenter/home_presenter.dart';
+import 'package:get/get.dart';
+
+class GetxHomePresenter extends GetxController implements HomePresenter {}
