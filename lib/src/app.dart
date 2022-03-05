8df699a4a6ce4f6b2a646440a5bce2ed19cd1039
Will Cavenagli <willcavenagli@gmail.com>
2022-03-05 09:59:04 -0300

    🧩: Add the Main Widget to its own separate file

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SizedBox(),
+    );
+  }
+}
