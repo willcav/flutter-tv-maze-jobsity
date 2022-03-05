b8625548c8726952f1673bd69e8ab26c9ff36dda
Will Cavenagli <willcavenagli@gmail.com>
2022-03-05 12:58:20 -0300

    🧩: Add new behavior to HomePresenter

--- lib/src/presentation/presenters/home_presenter/getx_home_presenter.dart
+++ lib/src/presentation/presenters/home_presenter/getx_home_presenter.dart
@@ -1,4 +1,33 @@
-import 'package:flutter_tv_maze_jobsity/src/presentation/presenters/home_presenter/home_presenter.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_tv_maze_jobsity/src/domain/entities/list_all_series/series_basic_info_entity.dart';
 import 'package:get/get.dart';
 
-class GetxHomePresenter extends GetxController implements HomePresenter {}
+import 'package:flutter_tv_maze_jobsity/src/domain/use_cases/list_all_series/get_all_series_paginated_use_case.dart';
+import 'package:flutter_tv_maze_jobsity/src/presentation/presenters/home_presenter/home_presenter.dart';
+
+class GetxHomePresenter extends GetxController implements HomePresenter {
+  final GetAllSeriesPaginatedUseCase getAllSeriesPaginatedUseCase;
+
+  GetxHomePresenter({
+    required this.getAllSeriesPaginatedUseCase,
+  });
+
+  final _seriesList = Rx<List<SeriesBasicInfoEntity>>([]);
+  final int _pageCounter = 0;
+
+  @override
+  void getAllSeries() async {
+    try {
+      final _params = GetAllSeriesPaginatedUseCaseParams(page: _pageCounter);
+
+      _seriesList.subject
+          .add(await getAllSeriesPaginatedUseCase.call(params: _params));
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  @override
+  Stream<List<SeriesBasicInfoEntity>> get seriesListStream =>
+      _seriesList.stream;
+}
--- lib/src/presentation/presenters/home_presenter/home_presenter.dart
+++ lib/src/presentation/presenters/home_presenter/home_presenter.dart
@@ -1,1 +1,7 @@
-abstract class HomePresenter {}
+import 'package:flutter_tv_maze_jobsity/src/domain/entities/list_all_series/series_basic_info_entity.dart';
+
+abstract class HomePresenter {
+  void getAllSeries();
+
+  Stream<List<SeriesBasicInfoEntity>> get seriesListStream;
+}
