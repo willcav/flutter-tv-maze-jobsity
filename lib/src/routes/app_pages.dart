55178ffebf864e3d38695782e63adf84172eda84
Will Cavenagli <willcavenagli@gmail.com>
2022-03-06 17:36:33 -0300

    🧩: Add SearchSeriesPage to Routing Handler

--- lib/src/routes/app_pages.dart
+++ lib/src/routes/app_pages.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import '../dependency_injection/factories/pages/episode_details_page_factory.dart';
 import '../dependency_injection/factories/pages/main_navigation_page_factory.dart';
 import '../dependency_injection/factories/pages/series_details_page_factory.dart';
+import '../ui/pages/search_series/search_series_page.dart';
 import 'app_routes.dart';
 
 class AppPages {
@@ -12,5 +13,7 @@ class AppPages {
     GetPage(name: AppRoutes.mainNavigationPage, page: makeMainNavigationPage),
     GetPage(name: AppRoutes.seriesDetailsPage, page: makeSeriesDetailsPage),
     GetPage(name: AppRoutes.episodeDetailsPage, page: makeEpisodeDetailsPage),
+    GetPage(
+        name: AppRoutes.searchSeriesPage, page: () => const SearchSeriesPage()),
   ];
 }
