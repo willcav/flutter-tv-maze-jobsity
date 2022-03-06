b48af9368e6695602db7b69b9b9859882f5a9e55
Will Cavenagli <willcavenagli@gmail.com>
2022-03-06 19:56:44 -0300

    🧩: Add Loading Manager Mixin for Presentation Layer

--- lib/src/presentation/presenters/search_series_presenter/getx_search_series_presenter.dart
+++ lib/src/presentation/presenters/search_series_presenter/getx_search_series_presenter.dart
@@ -1,17 +1,48 @@
 import 'package:flutter_tv_maze_jobsity/src/domain/entities/list_all_series/series_basic_info_entity.dart';
+import 'package:flutter_tv_maze_jobsity/src/domain/errors/domain_error.dart';
 import 'package:flutter_tv_maze_jobsity/src/domain/use_cases/search_series/search_series_by_name_use_case.dart';
+import 'package:flutter_tv_maze_jobsity/src/presentation/mixins/loading_manager.dart';
 import 'package:flutter_tv_maze_jobsity/src/presentation/presenters/search_series_presenter/search_series_presenter.dart';
 import 'package:get/get.dart';
 
 class GetxSearchSeriesPresenter extends GetxController
+    with LoadingManager
     implements SearchSeriesPresenter {
   final SearchSeriesByNameUseCase searchSeriesByNameUseCase;
 
-  GetxSearchSeriesPresenter({required this.searchSeriesByNameUseCase});
+  GetxSearchSeriesPresenter({required this.searchSeriesByNameUseCase}) {
+    isLoading = false;
+  }
 
   final _seriesList = Rx<List<SeriesBasicInfoEntity>>(const []);
 
   @override
   Stream<List<SeriesBasicInfoEntity>> get seriesListStream =>
       _seriesList.stream;
+
+  @override
+  void searchSeries(String input) async {
+    try {
+      if (input.length > 2) {
+        if (loadingState == false) {
+          isLoading = true;
+
+          final result =
+              await searchSeriesByNameUseCase.call(searchInput: input);
+
+          _seriesList.subject.add(result);
+
+          isLoading = false;
+        }
+      }
+
+      if (input.isEmpty) {
+        _seriesList.subject.add([]);
+        isLoading = false;
+      }
+    } on DomainError catch (e) {
+      isLoading = false;
+      _seriesList.addError(e.toString(), StackTrace.empty);
+    }
+  }
 }
--- lib/src/presentation/presenters/search_series_presenter/search_series_presenter.dart
+++ lib/src/presentation/presenters/search_series_presenter/search_series_presenter.dart
@@ -1,5 +1,9 @@
 import '../../../domain/entities/list_all_series/series_basic_info_entity.dart';
 
 abstract class SearchSeriesPresenter {
+  void searchSeries(String input);
+
   Stream<List<SeriesBasicInfoEntity>> get seriesListStream;
+
+  Stream<bool> get isLoadingStream;
 }
