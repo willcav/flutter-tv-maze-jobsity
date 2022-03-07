207699183528fe9bec32d13ece71f198290f3350
Will Cavenagli <willcavenagli@gmail.com>
2022-03-07 13:27:50 -0300

    ⭐: Add Use Case Implementation to Favorite Feature

---
+++ lib/src/data/use_cases/remote_get_all_favorite_series.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_tv_maze_jobsity/src/domain/entities/get_all_series/series_basic_info_entity.dart';
+import 'package:flutter_tv_maze_jobsity/src/domain/errors/domain_error.dart';
+import 'package:flutter_tv_maze_jobsity/src/domain/use_cases/fetch_all_favorite_series_ids/fetch_all_favorite_series_ids_use_case.dart';
+import 'package:flutter_tv_maze_jobsity/src/domain/use_cases/get_all_favorite_series/get_all_favorite_series_use_case.dart';
+import 'package:flutter_tv_maze_jobsity/src/domain/use_cases/get_series_by_id/get_series_by_id_use_case.dart';
+
+class RemoteGetAllFavoriteSeries implements GetAllFavoriteSeriesUseCase {
+  final FetchAllFavoriteSeriesIdsUseCase fetchAllFavoriteSeriesIdsUseCase;
+  final GetSeriesByIdUseCase getSeriesByIdUseCase;
+
+  RemoteGetAllFavoriteSeries({
+    required this.fetchAllFavoriteSeriesIdsUseCase,
+    required this.getSeriesByIdUseCase,
+  });
+
+  @override
+  Future<List<SeriesBasicInfoEntity>> call() async {
+    try {
+      final favoriteSeriesIds = await fetchAllFavoriteSeriesIdsUseCase.call();
+
+      if (favoriteSeriesIds.isEmpty) {
+        return [];
+      }
+
+      final favoriteSeriesList = <SeriesBasicInfoEntity>[];
+
+      for (var id in favoriteSeriesIds) {
+        final result = await getSeriesByIdUseCase.call(id: id);
+
+        favoriteSeriesList.add(result);
+      }
+
+      return favoriteSeriesList;
+    } on DomainError {
+      rethrow;
+    }
+  }
+}
