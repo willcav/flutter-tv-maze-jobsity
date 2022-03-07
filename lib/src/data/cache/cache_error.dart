c9ca1749863e659159e3b605ee94298b27e24c34
Will Cavenagli <willcavenagli@gmail.com>
2022-03-07 13:26:07 -0300

    🧩: Add Cache Exception Support

---
+++ lib/src/data/cache/cache_error.dart
@@ -0,0 +1,42 @@
+import '../../base/errors/local_error.dart';
+import '../../domain/errors/domain_error.dart';
+
+abstract class CacheError extends DomainError {
+  const CacheError._({String message = ''}) : super(message: message);
+
+  factory CacheError.failedToSave({String message = ''}) =>
+      _FailedToSave(message: message);
+  factory CacheError.failedToRetrieveData({String message = ''}) =>
+      _FailedToRetrieveData(message: message);
+  factory CacheError.failedToDelete({String message = ''}) =>
+      _FailedToDelete(message: message);
+}
+
+class _FailedToSave extends CacheError {
+  const _FailedToSave({String message = ''}) : super._(message: message);
+}
+
+class _FailedToRetrieveData extends CacheError {
+  const _FailedToRetrieveData({String message = ''})
+      : super._(message: message);
+}
+
+class _FailedToDelete extends CacheError {
+  const _FailedToDelete({String message = ''}) : super._(message: message);
+}
+
+extension ConvertToBaseError on CacheError {
+  /// Convert CacheError to BaseError equivalent
+  DomainError convertError() {
+    switch (runtimeType) {
+      case _FailedToSave:
+        return LocalError(message: message);
+      case _FailedToRetrieveData:
+        return LocalError(message: message);
+      case _FailedToDelete:
+        return LocalError(message: message);
+      default:
+        return LocalError(message: message);
+    }
+  }
+}
