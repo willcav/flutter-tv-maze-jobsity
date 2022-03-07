acb90d08bb49b686397eba1f867ab82194179cdf
Will Cavenagli <willcavenagli@gmail.com>
2022-03-07 13:27:22 -0300

    🧩: Add CacheException flow to SharedPreferences Adapter

--- lib/src/infra/cache/shared_preferences_adapter.dart
+++ lib/src/infra/cache/shared_preferences_adapter.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_tv_maze_jobsity/src/data/cache/cache_error.dart';
 import 'package:flutter_tv_maze_jobsity/src/data/cache/delete_string_list_data_storage.dart';
 import 'package:flutter_tv_maze_jobsity/src/data/cache/fetch_string_list_data_storage.dart';
 import 'package:flutter_tv_maze_jobsity/src/data/cache/save_string_list_data_storage.dart';
@@ -10,20 +12,35 @@ class SharedPreferencesAdapter
         DeleteStringListDataStorage {
   @override
   Future<bool> deleteList({required String key}) async {
-    final storage = await SharedPreferences.getInstance();
-    return storage.remove(key);
+    try {
+      final storage = await SharedPreferences.getInstance();
+      return storage.remove(key);
+    } catch (e) {
+      debugPrint(e.toString());
+      throw CacheError.failedToDelete();
+    }
   }
 
   @override
   Future<List<String>> fetchList({required String key}) async {
-    final storage = await SharedPreferences.getInstance();
-    return storage.getStringList(key) ?? [];
+    try {
+      final storage = await SharedPreferences.getInstance();
+      return storage.getStringList(key) ?? [];
+    } catch (e) {
+      debugPrint(e.toString());
+      throw CacheError.failedToRetrieveData();
+    }
   }
 
   @override
   Future<bool> saveList(
       {required String key, required List<String> value}) async {
-    final storage = await SharedPreferences.getInstance();
-    return storage.setStringList(key, value);
+    try {
+      final storage = await SharedPreferences.getInstance();
+      return storage.setStringList(key, value);
+    } catch (e) {
+      debugPrint(e.toString());
+      throw CacheError.failedToSave();
+    }
   }
 }
