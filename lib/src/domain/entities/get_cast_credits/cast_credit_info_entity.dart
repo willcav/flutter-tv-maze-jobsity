fe83cd7533e62871a898abb298fc3c591cfb1aa4
Will Cavenagli <willcavenagli@gmail.com>
2022-03-08 00:03:18 -0300

    🧩: Add Entities for Search People Feature

---
+++ lib/src/domain/entities/get_cast_credits/cast_credit_info_entity.dart
@@ -0,0 +1,9 @@
+class CastCreditInfoEntity {
+  final String showId;
+  final String characterId;
+
+  const CastCreditInfoEntity({
+    required this.showId,
+    required this.characterId,
+  });
+}
